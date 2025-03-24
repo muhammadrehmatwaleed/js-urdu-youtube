4187197a11de4032925a027f888ca659794f4e3a
muhammadrehmatwaleed <muhammadrehmatwaleed.367@gmail.com>
2025-03-24 07:30:54 +0000

    datatypes and var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 786786
+let accountEmail = "Rehmat367@gmail.com"
+var accountPassword = "123456" 
+accountCity = "Gojra"
+let accountState;
+
+// accountId = 2 //
+accountEmail = "rw@gmail.com"
+accountPassword = "142536"
+accountCity = "Fsd"
+
+
+/*
+var no use due to issue of block scope and funct scope
+*/
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountId,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" //treat all JS code as new version
+
+//alert(3+3) // we are using nodejs, no browser
+
+console.log(3
+    +3
+);// code readibilty should be high
+
+console.log("Rehmat");          //tc39.es ,alert mdn //
+
+let name = "waleed"
+let age = 20
+let isLoggedid = false
+let state;
+
+//number => 2 to power 53
+//bignit
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique value 
+
+//object
+
+console.log(typeof undefined);
+
+
