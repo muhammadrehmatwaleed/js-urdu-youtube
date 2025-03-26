3b769be9311a0a4a704bbd0914208c199447ef7c
muhammadrehmatwaleed <muhammadrehmatwaleed.367@gmail.com>
2025-03-26 09:11:53 +0000

    comparison and summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+// Primitive
+
+// 7 types : string, number, boolean, null, undefined ,bignit ,symbol
+
+
+
+// Reference type(non-primitive)
+
+//arrays ,objects , functions 
+const heroes = ["muhammad", "quaid-e-azam","allama iqbal"]
+
+let myObj = {
+    name:"rehmat",
+    age:22,
+}
+
+const myFunction = function () {
+    console.log("hello world");
+    
+}
+
+
